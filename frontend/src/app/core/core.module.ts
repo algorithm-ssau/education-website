b9719480eb777489303f1096e6449e0274c11178
zzz zz <keeperbel@gmail.com>
2024-03-30 22:39:58 +0400

    feat: Добавлено содержимое главной страницы

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, MainPageComponent],
   imports: [CommonModule],
-  exports: [HeaderComponent, FooterComponent],
+  exports: [HeaderComponent, FooterComponent, MainPageComponent],
 })
 export class CoreModule {}
