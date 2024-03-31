f3963bd87c50a9b8bac4dc59b982d8bfb945a18f
Dryyyys <odinborsonnorsegod@gmail.com>
2024-03-31 19:03:31 +0400

    build: добавлены классы моделей для взаимодействия с базой данных

--- backend/EducationWebsite/EducationWebsite/settings.py
+++ backend/EducationWebsite/EducationWebsite/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'EducationWebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': 'localhost',  # Может потребоваться изменить, если PostgreSQL находится на другом хосте
+        'PORT': '5432',
     }
 }
 
