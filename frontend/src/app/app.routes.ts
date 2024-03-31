0d8d5b306687d22d132f3305e3f668a6d98c910d
Quaqva <sanek15056@gmail.com>
2024-03-31 16:47:02 +0400

    feat: настроен роутинг для главной страницы, добавлен шаблон страницы 404

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,4 +1,15 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './core/pages/main-page/main-page.component';
+import { NotFoundPageComponent } from './core/pages/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainPageComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundPageComponent,
+  },
+];
 export default routes;
