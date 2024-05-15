a533e20f170b4b41ccb5fd42d79e616347c57213
Dryyyys <odinborsonnorsegod@gmail.com>
2024-05-15 15:11:00 +0400

    feat: добавлены форма и представление для регистрации пользователей

--- backend/EducationWebsite/EducationWebsite/urls.py
+++ backend/EducationWebsite/EducationWebsite/urls.py
@@ -2,11 +2,11 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.contrib import admin
 from django.urls import path
 
-from Registration.views import RegistUser
+from Registration.views import signup
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Registration/', RegistUser),
+    path('signup/', signup, name='signup'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
--- backend/EducationWebsite/Registration/views.py
+++ backend/EducationWebsite/Registration/views.py
@@ -1,5 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+from rest_framework_simplejwt.tokens import RefreshToken
 
-def RegistUser(request):
-    return HttpResponse('<h1>Ошибка 404: Тест на пидора не пройден<h1>')
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            email = form.cleaned_data.get('email')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(email=email, password=raw_password)
+            login(request, user)
+            
+            # Генерация JWT-токена
+            refresh = RefreshToken.for_user(user)
+            # Отправка токенов в ответ на успешную регистрацию
+            return render(request, 'registration/signup_success.html', {'refresh': str(refresh), 'access': str(refresh.access_token)})  
+            
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
