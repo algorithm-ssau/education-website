9af85fd388fa0c2ff6bbb1b8edf5ca08ceaef98a
Quaqva <sanek15056@gmail.com>
2024-05-17 20:42:59 +0400

    refactor: изменения в компоненте бокового меню

--- frontend/src/app/shared/components/sidebar/sidebar.component.ts
+++ frontend/src/app/shared/components/sidebar/sidebar.component.ts
@@ -6,6 +6,9 @@ import {
   ElementRef,
   Input,
 } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import CheckMobile from '../../utils/is-mobile';
 
 @Component({
   selector: 'app-sidebar',
@@ -15,8 +18,13 @@ import {
   styleUrl: './sidebar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
+@UntilDestroy()
 export class SidebarComponent {
   @Input() public side: 'left' | 'right' = 'left';
+
+  @Input() public closeOnDesktop: boolean = false;
+
+  @CheckMobile() isMobile$!: Observable<boolean>;
 
   public isOpen: boolean = false;
 
@@ -25,19 +33,25 @@ export class SidebarComponent {
   public constructor(
     private elementRef: ElementRef,
     private changeDetector: ChangeDetectorRef,
-  ) {}
+  ) {
+    if (this.closeOnDesktop) {
+      this.isMobile$.pipe(untilDestroyed(this)).subscribe((isMobile) => {
+        if (!isMobile) this.close();
+      });
+    }
+  }
 
   public open(): void {
     this.isOpen = true;
-    this.playAnimation();
+    this.animateChangingState();
   }
 
   public close(): void {
     this.isOpen = false;
-    this.playAnimation();
+    this.animateChangingState();
   }
 
-  private playAnimation(): void {
+  private animateChangingState(): void {
     this.isAnimating = true;
     this.changeDetector.detectChanges();
     this.elementRef.nativeElement.addEventListener(
