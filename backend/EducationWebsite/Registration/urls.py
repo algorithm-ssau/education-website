b89d6ab7295046e2f40c3ccdaaf5b41e45c334a3
Dryyyys <odinborsonnorsegod@gmail.com>
2024-05-20 00:43:12 +0400

    feat: добавлено представление для входа зарегистрированных пользователей

--- backend/EducationWebsite/Registration/urls.py
+++ backend/EducationWebsite/Registration/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import RegistrationAPIView
+from .views import LoginAPIView, RegistrationAPIView
 
 app_name = 'Registration'
 urlpatterns = [
     path('users/', RegistrationAPIView.as_view()),
+    path('users/login/', LoginAPIView.as_view()),
 ]
--- backend/EducationWebsite/Registration/views.py
+++ backend/EducationWebsite/Registration/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .renderers import UserJSONRenderer
-from .serializers import RegistrationSerializer
+from .serializers import LoginSerializer, RegistrationSerializer
 
 class RegistrationAPIView(APIView):
     permission_classes = (AllowAny,)
@@ -17,3 +17,16 @@ class RegistrationAPIView(APIView):
         serializer.save()
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class LoginAPIView(APIView):
+    permission_classes = (AllowAny,)
+    renderer_classes = (UserJSONRenderer,)
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        user = request.data.get('user', {})
+
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
