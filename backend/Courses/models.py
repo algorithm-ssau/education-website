b9f0b2d4e4162ef775fa188e53a3269f7a91a665
minekraftKriperBoi <desoll112@yandex.ru>
2024-05-24 01:00:36 +0400

    feat: добавлены классы моделей для создания блоков внутри курса

--- backend/Courses/models.py
+++ backend/Courses/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from Registration.models import User
 
-# Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='courses')
+
+    def __str__(self):
+        return self.title
+
+class TheoryBlock(models.Model):
+    id_page =  models.UUIDField(primary_key=True)
+    course = models.ForeignKey(Course, related_name='theory_blocks', on_delete=models.CASCADE)
+    text = models.TextField(blank=True)
+    video_url = models.URLField(blank=True)
+    image = models.ImageField(upload_to='theory_images/', blank=True)
+    captioned_image = models.ImageField(upload_to='captioned_images/', blank=True)
+    caption = models.CharField(max_length=300, blank=True)
+    page_number = models.IntegerField(default=1)
+
+class TaskBlock(models.Model):
+    course = models.ForeignKey(Course, related_name='task_blocks', on_delete=models.CASCADE)
+    task_text = models.TextField()
+    comment = models.TextField(blank=True)
